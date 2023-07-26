e6713234b76126e995dab9906f53832af3d208c8
happymink <minkyun@kakao.com>
2023-07-26 21:38:54 +0900

    Initial commit

---
+++ src/main/java/com/jojoldu/book/himk/HimkApplication.java
@@ -0,0 +1,13 @@
+package com.jojoldu.book.himk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HimkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HimkApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jojoldu/book/himk/HimkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jojoldu.book.himk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HimkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
